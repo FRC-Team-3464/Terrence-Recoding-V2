276a6c393b1fa43424f8d04936af1f9b4f70da5d
Legion <Legion>
2024-11-14 17:55:45 -0500

    Code Arcade and Tank Drive

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -14,10 +15,71 @@ public class DriveSubsystem extends SubsystemBase {
   /** Creates a new DriveSubsystem. */
   
   private static CANSparkMax leftFront = new CANSparkMax( 5, MotorType.kBrushless);
+  private static CANSparkMax leftBack = new CANSparkMax(6, MotorType.kBrushless);
   private static CANSparkMax rightFront = new CANSparkMax(7, MotorType.kBrushless);
+  private static CANSparkMax rightBack = new CANSparkMax(8, MotorType.kBrushless);
+
+
   public static DifferentialDrive drive = new DifferentialDrive(leftFront, rightFront);
 
-  public DriveSubsystem() {}
+  public DriveSubsystem() {
+    leftFront.setInverted(true);
+  }
+
+
+  public void tankDrive(double left, double right) {
+
+    if(Math.abs(left) <= 0.07) {
+      left = 0;
+    }
+    if(Math.abs(right)<=0.07){
+      right=0;
+    }
+
+
+    drive.tankDrive(left, right);
+
+    leftBack.follow(leftFront);
+    rightBack.follow(rightFront);
+
+  }
+
+  
+  public void arcadeDrive(double speed, double rotation) {
+    drive.arcadeDrive(speed, rotation); 
+
+    leftBack.follow(leftFront);
+    rightBack.follow(rightFront);
+  }
+
+
+  public void stopDrive() {
+    arcadeDrive(0, 0);
+  }
+
+
+  public double getLeft(){
+    return leftFront.get();
+  }
+
+  public double getRight() {
+    return rightFront.get();
+  }
+
+  public void enableMotors(boolean on){
+    IdleMode mode;
+    if (on) {
+      mode = IdleMode.kBrake;
+    }
+    else{
+      mode = IdleMode.kCoast;
+    } 
+    //set motors to mode
+    leftFront.setIdleMode(mode);
+    leftBack.setIdleMode(mode);
+    rightFront.setIdleMode(mode);
+    rightBack.setIdleMode(mode);
+  }
 
   @Override
   public void periodic() {
